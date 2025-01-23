4638a2c42009a93db06ec31bce618b68b5be84f3
Moon Dev <115879186+moondevonyt@users.noreply.github.com>
2025-01-23 12:46:01 -0400

    repplit

--- replit.nix
+++ replit.nix
@@ -1,7 +1,12 @@
 { pkgs }: {
     deps = [
-        pkgs.python3
-        pkgs.python3Packages.pip
+        pkgs.python310Full
+        pkgs.python310Packages.fastapi
+        pkgs.python310Packages.uvicorn
+        pkgs.python310Packages.pip
+        pkgs.python310Packages.python-multipart
+        pkgs.python310Packages.jinja2
+        pkgs.python310Packages.python-dotenv
         pkgs.nodejs
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
